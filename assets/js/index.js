a4f6ba418d15ea01d78cbc04424f28a7d2335291
BlackOne <surnin.vladislav@gmail.com>
2025-04-19 16:37:44 +0300

    добавление комментариев к коду

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,27 @@
+// Импорт функций для инициализации слайдеров, этапов, модального окна и видео
 import { slidersInit } from "./sliders.js";
 import { stagesInit } from './stages.js';
 import { openModal } from "./modal.js";
 import { openVideo } from './video.js';
 
+// Инициализация слайдеров
 slidersInit();
+
+// Инициализация этапов
 stagesInit();
 
+// Добавление обработчиков событий для кнопок с классом "button--external"
+// При клике открывается модальное окно
 document.querySelectorAll('.button--external').forEach(button => {
   button.addEventListener('click', openModal);
 });
 
+// Добавление обработчиков событий для кнопок с классом "nominations__button"
+// При клике открывается модальное окно
 document.querySelectorAll('.nominations__button').forEach(button => {
   button.addEventListener('click', openModal);
 });
 
+// Добавление обработчика события для элемента с классом "video__player"
+// При клике запускается функция открытия видео
 document.querySelector('.video__player').addEventListener('click', openVideo);
